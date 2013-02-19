9e36b5d2d28a9ad463f69bd6b0233649de013142
Martin Akolo <chiteri@geek.co.ke>
2013-02-19 10:43:05 +0300

    Fixed the issue with serving static files on the site.

--- tyre_tracking/urls.py
+++ tyre_tracking/urls.py
@@ -1,22 +1,19 @@
 from django.conf.urls import patterns, include, url
-from tyre_tracking.views import hello, current_datetime, hours_ahead, display_meta 
+import os 
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^hello/$', hello), 
-    url(r'^time/$', current_datetime), 
-    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
-    url(r'^meta/$', display_meta),
     # Examples:
-    # url(r'^$', 'tyre_tracking.views.home', name='home'),
+    url(r'^$', 'tyre_tracking.views.home', name='home'),
     # url(r'^tyre_tracking/', include('tyre_tracking.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)), 
+    #(r'^$', home),
 )
--- tyre_tracking/views.py
+++ tyre_tracking/views.py
@@ -1,32 +1,36 @@
-from django.http import Http404, HttpResponse 
-from django.template import Template, Context
-import datetime 
+#from django.http import Http404, HttpResponse 
+#from django.template import Template, Context
+from django.views.generic.simple import direct_to_template
+#import datetime 
 
-def hello(request): 
-    return HttpResponse("Hello World!")
+def home(request): 
+    return direct_to_template(request, 'base.html') 
 
-def current_datetime(request): 
-    now = datetime.datetime.now() 
-    t = Template("<html><body>It is now {{ current_date }}. </body></html>")
-    html = t.render(Context ({'current_date':now})) 
-    return HttpResponse(html) 
+#def hello(request): 
+#    return HttpResponse("Hello World!")
 
-def hours_ahead(request, offset): 
-    try: 
-        offset = int(offset) 
-    except ValueError: 
-        raise Http404()
+#def current_datetime(request): 
+#    now = datetime.datetime.now() 
+#    t = Template("<html><body>It is now {{ current_date }}. </body></html>")
+#    html = t.render(Context ({'current_date':now})) 
+#    return HttpResponse(html) 
 
-    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s) it will be %s</body></html>"%(offset, dt)
-    return HttpResponse(html)
+#def hours_ahead(request, offset): 
+#    try: 
+#        offset = int(offset) 
+#    except ValueError: 
+#        raise Http404()
 
-def display_meta(request):
-    values = request.META.items() 
-    values.sort() 
-    html = [] 
+#    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+#    html = "<html><body>In %s hour(s) it will be %s</body></html>"%(offset, dt)
+#    return HttpResponse(html)
 
-    for k, v in values: 
-        html.append('<tr><td>%s</td><td>%s</td></tr>'%(k, v))
+#def display_meta(request):
+#    values = request.META.items() 
+#    values.sort() 
+#    html = [] 
 
-    return HttpResponse('<table>%s</table>' % '\n'.join(html))
+#    for k, v in values: 
+#        html.append('<tr><td>%s</td><td>%s</td></tr>'%(k, v))
+
+#    return HttpResponse('<table>%s</table>' % '\n'.join(html))

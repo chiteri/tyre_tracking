6d0e1f284a78ce21a4714054d2df41ccae171d67
Martin Akolo <chiteri@geek.co.ke>
2013-03-01 12:50:34 +0300

    Field vehicle_from removed from tyre as it is redundant. Default css file to the site added.

--- tyre_tracking/transfer/forms.py
+++ tyre_tracking/transfer/forms.py
@@ -8,7 +8,7 @@ class TransferForm(forms.ModelForm):
         modified_tyre = data["tyre"] 
         	
         # Make sure that the tyre is moving to a new position, if on the same vehicle 
-        if data["vehicle_from"] == data["vehicle_to"] and modified_tyre.position_fitted == data["new_position"]: 
+        if modified_tyre.vehicle_fitted == data["vehicle_to"] and modified_tyre.position_fitted == data["new_position"]: 
             raise forms.ValidationError('Tyre is moving from [ %s ] to [ %s ] which is identical'% (modified_tyre.position_fitted, data["new_position"]))
 					
         # If missing vehicle to, then tyre's new position is not a-must but its status is mandatory and should change (retread, written off) 
@@ -17,14 +17,16 @@ class TransferForm(forms.ModelForm):
         elif not data["vehicle_to"] and not data["tyre_state"]:
             raise forms.ValidationError('Tyre cannot go miss both a vehicle to and a new status.') 
 				
-        # If missing vehicle from, then tyre's new position is mandatory but the status is not. It will pick the last one  
-        if not data["vehicle_from"] and not data["vehicle_to"] and not data["new_position"]: 		
-            raise forms.ValidationError('A tryre from the repair / retred needs to have a new position and vehicle') 
-        elif data["vehicle_to"] and not data["new_position"]:			
+        # You should specify the new position of a tyre when assigning it to a vehicle
+        if data["vehicle_to"] and not data["new_position"]:			
             raise forms.ValidationError('You should specify a new tyre position on %s.'%data["vehicle_to"]) 
 			
-        # A tyre that has been written off cannot be move to another vehicle, just from 
-        if modified_tyre.status == Tyre.TYRE_STATUS_CHOICES[2][0] : 
+		# A tyre that is being written off cannot be assigned to a vehicle  
+        if data["tyre_state"] == Tyre.TYRE_STATUS_CHOICES[2][0] and data["vehicle_to"]: 
+            raise forms.ValidationError('A Tyre that is %s cannot be assigned to %s.'%(Tyre.TYRE_STATUS_CHOICES[2][1], data["vehicle_to"]))
+			
+        # A tyre that has been written off cannot be move to another vehicle, only from 
+        if modified_tyre.status == Tyre.TYRE_STATUS_CHOICES[2][0]: 
             raise forms.ValidationError('Tyre %s is written off and cannot be put back on %s.'%(modified_tyre.serial_number, data["vehicle_to"]))
 			
         return data 
--- tyre_tracking/transfer/models.py
+++ tyre_tracking/transfer/models.py
@@ -9,7 +9,7 @@ class Transfer(models.Model):
     agent = models.ForeignKey(User, help_text = "The person who effected the transfer", unique=False, related_name='transfer_agent')
     transfer_date = models.DateField(help_text="Date transfer was effected", blank=False, null=False, default=datetime.datetime.now) 
     tyre =  models.ForeignKey(Tyre, help_text="Tyre transferred") 
-    vehicle_from = models.ForeignKey(Vehicle, related_name="transfer_from", null=True, blank=True) 
+    # vehicle_from = models.ForeignKey(Vehicle, related_name="transfer_from", null=True, blank=True) 
     vehicle_to = models.ForeignKey(Vehicle, related_name="transfer_to", null=True, blank=True) 
     new_position = models.CharField(max_length=4, choices = Tyre.TYRE_POSITION_CHOICES, null=True, blank=True) 
     tyre_state =  models.CharField(max_length=1, choices = Tyre.TYRE_STATUS_CHOICES)
